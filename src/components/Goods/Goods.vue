fb539a723814080e16759c4bc0433b43bd9d7a7e
JunYanLin <ruky8459@gmail.com>
2021-01-25 15:49:16 +0800

    獲取數據

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -14,7 +14,30 @@
 
 <script>
     export default{
+        data(){
+            return{
+                container:{},
+                goods:[]
+            }
+        },
+        created() {
+            var that = this;
 
+            this.$axios.get('api/goods')
+                .then(function(response){
+                    var dataSource = response.data;
+                    if(dataSource.data.code == 0){
+                        // console.log(dataSource.data);
+                        that.container = dataSource.data.data.container_operation_source;
+                        that.goods = dataSource.data.data.food_spu_tags;
+                        console.log(that.container);
+                        console.log(that.goods);
+                    }
+                })
+                .catch(function(error){
+                    console.log(error);
+                })
+        }
     }
 </script>
 
