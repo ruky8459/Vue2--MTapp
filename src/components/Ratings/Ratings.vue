9f5775f97632f9cdcc1c5dd68327af31f5003021
JunYanLin <ruky8459@gmail.com>
2021-02-18 23:06:40 +0800

    評論頁面完成

--- src/components/Ratings/Ratings.vue
+++ src/components/Ratings/Ratings.vue
@@ -1,4 +1,5 @@
 <template>
+<div class="ratings-wrapper" ref="rtScroll">
     <div class="ratings">
         <div class="overview">
             <div class="overview-left">
@@ -65,25 +66,29 @@
                                 {{ comment.user_name }}
                             </div>
                             <div class="time">
-                                {{ comment.comment_time }}
+                                {{ formatDate(comment.comment_time) }}
                             </div>
                         </div>
                         <div class="star-wrapper">
                             <span class="text">評分</span>
                             <Star :score="comment.order_comment_score"></Star>
                         </div>
-                        <div class="content">
-                            {{ comment.comment }}
+                        <div class="comment-content" v-html="commentStr(comment.comment)">
+                        </div>
+                        <div class="img-wrapper" v-if="comment.comment_pics.length">
+                            <img v-for="item in comment.comment_pics" :key="item.id" :src="item.thumbnail_url">
                         </div>
                     </div>
                         </li>
             </ul>
         </div>
     </div>
+</div>
 </template>
 <script>
 import Star from "components/Star/Star";
 import Split from "components/Split/Split";
+import BScroll from 'better-scroll';
 
 const ALL = 2 ; //類型選擇: 全部
 const PICTURE = 1 ;//類型選擇: 有圖
@@ -104,6 +109,18 @@ export default {
                 let dataSource = response.data.data;
                 if (dataSource.code == 0) {
                     that.ratings = dataSource.data;
+
+                    //滾動初始化
+                    that.$nextTick(()=>{
+                        if(!that.Scroll){
+                            that.Scroll = new BScroll(that.$refs.rtScroll,{
+                                click: true
+                            });
+                        }else{
+                            that.Scroll.refresh();
+                        }
+                    });
+
                 }
             })
             .catch(function (error) {
@@ -113,14 +130,42 @@ export default {
     methods:{
         selectTypeFn(type){
             this.selectType = type ;
+            //刷新
+            this.$nextTick(()=>{
+                this.Scroll.refresh();
+            })
+        },
+        formatDate(time){
+            let date = new Date(time*1000);
+            // 格式
+            let fmt = 'yyyy.MM.dd'
+
+            if(/(y+)/.test(fmt)){
+                let year = date.getFullYear().toString();
+                fmt = fmt.replace(RegExp.$1,year);
+            }
+            if(/(M+)/.test(fmt)){
+                let month = date.getMonth().toString();
+                fmt = fmt.replace(RegExp.$1,month);
+            }
+            if(/(d+)/.test(fmt)){
+                let day = date.getDate().toString();
+                fmt = fmt.replace(RegExp.$1,day);
+            }
+            return fmt ;
+        },
+        commentStr(content){
+            let rel = /#[^#]+#/g;
+
+            return content.replace(rel, '<i>$&</i>');
         }
     },
     computed:{
         selectComment(){
-            if(this.selectType = ALL ){ //所有評價
+            if(this.selectType == ALL ){ //所有評價
                 return this.ratings.comments;
             }else if(this.selectType == PICTURE){ //有圖評價
-                let arr=[];
+                let arr = [];
                 this.ratings.comments.forEach((comment)=>{
                     if(comment.comment_pics.length){
                         arr.push(comment);
@@ -134,7 +179,8 @@ export default {
     },
     components: {
         Star,
-        Split
+        Split,
+        BScroll
     }
 };
 </script>
